2e200ccac32abb08ce76749472ef3d6ea20db057
今野郷 <xiangjinye1@gmail.com>
2020-08-09 03:51:44 +0900

    部分テンプレート作成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update]
+
     def home
 
     end
@@ -8,7 +10,6 @@ class ArticlesController < ApplicationController
     end
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def new
@@ -26,11 +27,9 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
         if @article.update(article_params)
             redirect_to article_path(@article), notice: '更新しました'
         else
@@ -46,6 +45,10 @@ class ArticlesController < ApplicationController
     end
 
     private
+    def set_article 
+        @article = Article.find(params[:id])
+    end
+
     def article_params
         params.require(:article).permit(:title, :content)
     end
