f0ba1889db7c399f338b7e33938c300cf8020797
今野郷 <xiangjinye1@gmail.com>
2020-08-14 17:39:21 +0900

    いいね機能、いいね一覧追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -14,6 +14,8 @@
 #  index_articles_on_user_id  (user_id)
 #
 class Article < ApplicationRecord
+    has_one_attached :eyecatch
+
     validates :title, presence: true
     validates :title, length: {minimum: 2, maximum: 100}
     validates :title, format: {with: /\A(?!\@)/}
@@ -22,10 +24,15 @@ class Article < ApplicationRecord
     validates :content, uniqueness: true
 
     has_many :comments, dependent: :destroy
+    has_many :likes, dependent: :destroy
     belongs_to :user
 
     def author_name
         user.display_name
+    end
+
+    def like_count
+        likes.count
     end
 
     def display_created_at
